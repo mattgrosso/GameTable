821cfec0da2678090cd5345307172c73c11d73a6
MJG <MJG@MacBook-Pro.local>
2016-05-17 10:28:12 -0400

    login messages are hiding and showing pretty well. I'll come back to it in a minute.

--- build/js/main.js
+++ build/js/main.js
@@ -828,16 +828,27 @@ LoginController.$inject = ['GameFactory', '$localStorage'];
     this.storedUsername = $localStorage.username;
     this.message = "";
 
+    this.loggedIn = false;
+
+    console.log('This is $localStorage.username: ',$localStorage.username);
+    if ($localStorage.username) {
+      console.log('Were inside of the if statement');
+      this.loggedIn = true;
+    }
+
     this.login = function login() {
       $localStorage.collection = null;
       GameFactory.getUserCollection(that.username)
         .then(function () {
           $localStorage.username = that.username;
+          that.loggedIn = true;
           that.message = "You are now logged in.";
+          that.username = "";
         })
         .catch(function () {
           that.message = "Log in failed. Please check your username.";
         });
+        that.message = "Please hold, BGG is slow.";
 
     };
   }
