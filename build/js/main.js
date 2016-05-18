a4e3767b25cbd12ddd63f94aa57f03faa54899e1
MJG <MJG@MacBook-Pro.local>
2016-05-18 13:20:23 -0400

    Adjusted directive to take in any message for display

--- build/js/main.js
+++ build/js/main.js
@@ -170,9 +170,9 @@
     return {
       restrict: 'A',
       template: '',
-      link: function renderPopUp(scope, element) {
+      link: function renderPopUp(scope, element, attrs) {
         element.click(function() {
-          element.after('<aside class="vote-popup">voted!</aside>');
+          element.after('<aside class="vote-popup">' + attrs + '</aside>');
           setTimeout(function () {
             $('.vote-popup').remove();
           }, 500);
