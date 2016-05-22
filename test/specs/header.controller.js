6a643e6c2e728deb134ec89bc3f6700e474ac577
MJG <MJG@MacBook-Pro.local>
2016-05-22 16:39:11 -0400

    made a few tests for the header controller

--- test/specs/header.controller.js
+++ test/specs/header.controller.js
@@ -8,6 +8,10 @@
     var headerController;
     var $rootScope;
     var mockGameFactory = {};
+    var localStorage = {
+      username: 'this is a username',
+      collection: 'this is a collection'
+    };
 
     setup(module('game'));
 
@@ -20,11 +24,26 @@
       mockGameFactory.amILoggedIn = function () {
         return true;
       };
+      mockGameFactory.logOut = function () {
+        localStorage.username = null;
+        localStorage.collection = null;
+      };
       headerController = $controller('HeaderController');
     }));
 
     test('sanity check', function (){
       assert.strictEqual(headerController.loggedIn, mockGameFactory.amILoggedIn, 'loggedIn function exists');
+    });
+
+    test('logOut function logs out', function () {
+      assert.strictEqual(localStorage.username, 'this is a username', 'the variable is set');
+      assert.strictEqual(localStorage.collection, 'this is a collection', 'the variable is set');
+
+      headerController.logOut();
+
+      assert.isNull(localStorage.username, 'logOut function sets username to null');
+      assert.isNull(localStorage.collection, 'logOut function sets collection to null');
+
     });
 
   });
