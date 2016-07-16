0656a48b5f88cf2dd79c6f8357d8560ba61124a9
MJG <MJG@MacBook-Pro.local>
2016-07-16 18:45:32 -0400

    cleaned up a few console.log()s

--- build/js/main.js
+++ build/js/main.js
@@ -183,7 +183,6 @@
             }
           }
           if (duration && isNaN(Number(duration)) && duration.includes('-')) {
-            console.log('in the correct filter');
             var durationWithoutSpaces = duration.split(' ').join('');
             var durationRangeArray = durationWithoutSpaces.split('-');
             var minDuration = 0;
@@ -199,8 +198,6 @@
             });
 
             if (minDuration > ((each.playTime.min + each.playTime.max)/2) || maxDuration < ((each.playTime.min + each.playTime.max)/2)) {
-              console.log('Min: ', each.playTime.min);
-              console.log('Max: ', each.playTime.max);
               include = false;
             }
           }
